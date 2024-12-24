ddccda29052f98da81fd39f28c5a86f745ed6738
Ivy Tanui <ivy.tanui@strathmore.edu>
2024-12-24 19:38:01 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+// Dark mode toggle
 document.getElementById('toggle-switch').addEventListener('click', () => {
     document.body.classList.toggle('dark');
+    // Toggle icon between moon and sun
+    const icon = document.querySelector('#toggle-switch i');
+    if (document.body.classList.contains('dark')) {
+        icon.classList.replace('fa-moon', 'fa-sun'); // Change icon to sun
+    } else {
+        icon.classList.replace('fa-sun', 'fa-moon'); // Change icon to moon
+    }
 });
 
-document.querySelector('.menu-button').addEventListener('click', () => {
+// Mobile menu toggle
+function toggleMenu() {
     const navMenu = document.getElementById('navMenu');
     navMenu.classList.toggle('responsive');
-});
+}
+
